ac0247cab66f1a848bbf0640e8dac4a5fd5ca968
Dominik Horvath <dom.horvath17@gmail.com>
2022-11-15 23:21:03 +0100

    small improvements

--- main.py
+++ main.py
@@ -16,14 +16,18 @@ if not mc.is_connected():
 
 consumer = KafkaConsumer("NEW_ENTRY", bootstrap_servers=os.environ.get("KAFKA_URL"))
 
-
+# TODO: use the database for updating the flag in the DB. if you dont do this, there is a race condition bug :(
 print("Listening for Kafka events...")
 for msg in consumer:
-    data = json.loads(msg.value.decode("utf-8"))
-    print("Consuming " + data["path"])
-    path = mc.save_object(data["bucket"], data["path"])
-    extractor = MetadataExtractor()
-    metadata = extractor.extract_using_droid(path)
-    metadata["stage"] = "done"
-    mc.update_object_metadata(data["bucket"], data["path"], metadata)
-    print("Done.")
+    try:
+        data = json.loads(msg.value.decode("utf-8"))
+        print("Consuming " + data["path"])
+        path = mc.save_object(data["bucket"], data["path"])
+        extractor = MetadataExtractor()
+        metadata = extractor.extract_using_droid(path)
+        metadata["stage"] = "done"
+        mc.update_object_metadata(data["bucket"], data["path"], metadata)
+        print("Done.")
+    except Exception as e:
+        print(e)
+        continue
